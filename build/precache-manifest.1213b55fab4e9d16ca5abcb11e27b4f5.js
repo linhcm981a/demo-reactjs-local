cd0839455d8221d968eef3e8d3035964cff2b95c
luuminhthai <luu.minhthai09@gmail.com>
2021-05-23 21:21:57 +0700

    up-

---
+++ build/precache-manifest.1213b55fab4e9d16ca5abcb11e27b4f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85a1196b116a220b4c07bef7a3a17d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ea495102eb6a3001d7f",
+    "url": "/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "5786e8ce2b04225ca9c1",
+    "url": "/static/css/main.db430fef.chunk.css"
+  },
+  {
+    "revision": "7ea495102eb6a3001d7f",
+    "url": "/static/js/2.8faa1640.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.8faa1640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5786e8ce2b04225ca9c1",
+    "url": "/static/js/main.228ef193.chunk.js"
+  },
+  {
+    "revision": "fe43d60bbb01d32c51d1",
+    "url": "/static/js/runtime-main.a3a8a1a1.js"
+  }
+]);
